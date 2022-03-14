3f9e6085d2510370badf22107172d07dc9cf37c8
Ollie <feoktistov.01@mail.ru>
2022-03-15 01:38:41 +1000

    changed task 5 from homework 2

--- hw2/hw5.py
+++ hw2/hw5.py
@@ -12,20 +12,6 @@ import string
 from typing import List, Any
 
 
-def custom_range(**kwargs) -> List[Any]:
-    line = kwargs["iter_object"]
-    stop = kwargs["stop"]
-    if len(kwargs) == 2:
-        return line[:line.index(stop)]
-    elif len(kwargs) == 3:
-        start = kwargs["start"]
-        return line[line.index(start):line.index(stop)]
-    elif len(kwargs) == 4:
-        step = kwargs["step"]
-        start = kwargs["start"]
-        return line[line.index(start):line.index(stop):step]
-
-
 def custom_range2(line, stop, start=None, step=None) -> List[Any]:
     if step is None:
         return line[:line.index(stop)]
@@ -38,11 +24,7 @@ def custom_range2(line, stop, start=None, step=None) -> List[Any]:
         3) line, start, stop and step""")
 
 
-# test_dict = {'iter_object': string.ascii_lowercase, 'stop': 'z', 'start': 'a', 'step': 2}
-test_dict = {'iter_object': string.ascii_lowercase, 'stop': 'a', 'start': 'z', 'step': -1}
-print(custom_range(**test_dict))  # calling the first option of the custom_range function
-
 try:
-    print(custom_range2(string.ascii_lowercase, 'h', step=2))  # calling the second option of the custom_range function
+    print(custom_range2(string.ascii_lowercase, 'h', step=2))
 except TypeError as er:
     print(er)
