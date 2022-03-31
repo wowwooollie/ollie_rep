4dbdb0c6939d11cd87b91124ffa3b9d3893f39d1
Ollie <feoktistov.01@mail.ru>
2022-04-01 00:27:57 +1000

    changed task 5 from homework2

--- hw2/hw5.py
+++ hw2/hw5.py
@@ -13,8 +13,10 @@ from typing import List, Any
 
 
 def custom_range2(line, stop, start=None, step=None) -> List[Any]:
-    if step is None:
+    if start is None and step is None:
         return line[:line.index(stop)]
+    elif step is None and start is not None:
+        return line[line.index(start):line.index(stop)]
     elif start is not None and step is not None:
         return line[line.index(start):line.index(stop):step]
     else:
