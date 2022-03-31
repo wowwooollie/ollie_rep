1b1c676bc2f0a21a5ee1d7a961f1e866b7b58e52
Ollie <feoktistov.01@mail.ru>
2022-04-01 00:35:35 +1000

    changed task 5 from homework2

--- hw2/hw5.py
+++ hw2/hw5.py
@@ -8,25 +8,15 @@ assert = custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd', 'e', 
 assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
 assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
 """
-import string
 from typing import List, Any
 
 
 def custom_range2(line, stop, start=None, step=None) -> List[Any]:
     if start is None and step is None:
         return line[:line.index(stop)]
-    elif step is None and start is not None:
-        return line[line.index(start):line.index(stop)]
     elif start is not None and step is not None:
         return line[line.index(start):line.index(stop):step]
+    elif step is None and start is not None:
+        return line[line.index(start):line.index(stop)]
     else:
-        raise TypeError("""the function takes following sets of arguments:
-        1)line and stop
-        2)line, start and stop
-        3) line, start, stop and step""")
-
-
-try:
-    print(custom_range2(string.ascii_lowercase, 'h', step=2))
-except TypeError as er:
-    print(er)
+        return line[:line.index(stop):step]
